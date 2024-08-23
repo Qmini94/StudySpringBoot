006f6d2caaf5805b58728e32becd69f0c5591c9d
박규민 <pkm1614@gmail.com>
2024-08-23 15:53:11 +0900

    first commit.

---
+++ src/test/java/kumin/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kumin.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
